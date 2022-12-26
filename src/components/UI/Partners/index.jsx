b3085aa1b3d245bd2fda6532474a51ebe27fddbf
Bobur_Sunnatov <bsunnatov135@gmail.com>
2022-12-26 13:07:59 +0500

    partners updated

--- src/components/UI/Partners/index.jsx
+++ src/components/UI/Partners/index.jsx
@@ -1,14 +1,25 @@
 import { Container } from "@mui/material";
 import styles from "./style.module.scss";
-import { AcerIcon, AsusIcon, MiIcon, SonyIcon, VivoIcon } from "../svg";
+import HydroIcon from "/public/logos/HydroIcon.svg";
+import FAOIcon from "/public/logos/FAOIcon.svg";
+import MedIcon from "/public/logos/MedIcon.svg";
+import GasLogo from "/public/logos/GasLogo.svg";
+import Unicef from "/public/logos/Unicef.svg";
+import Tree from "/public/logos/Tree.svg";
+import UNDP from "/public/logos/UNDP.svg";
+import EduIcon from "/public/logos/EduIcon.svg";
+import UNEP from "/public/logos/UNEP.svg";
 
 const items = [
-  <AcerIcon />,
-  <SonyIcon />,
-  <AsusIcon />,
-  <MiIcon />,
-  <VivoIcon />,
-  <VivoIcon />,
+  <MedIcon />,
+  <GasLogo />,
+  <Unicef />,
+  <HydroIcon />,
+  <FAOIcon />,
+  <Tree />,
+  <UNDP />,
+  <EduIcon />,
+  <UNEP />,
 ];
 
 export default function Partners() {
