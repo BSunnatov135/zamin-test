52b277923a34f7ea5de05be3fde0aae58fde262b
Bobur_Sunnatov <bsunnatov135@gmail.com>
2023-01-31 11:43:48 +0500

    bug fix

--- src/components/UI/Header/Menu/ProjectItems/projects.jsx
+++ src/components/UI/Header/Menu/ProjectItems/projects.jsx
@@ -6,7 +6,7 @@ export default function Projects({ item, handleClose }) {
   const { lang } = useTranslation();
   return (
     <div key={item.guid}>
-      <Link href={"/"}>
+      <Link href={`/project-info/${item.guid}`}>
         <a
           className={styles.projectItem}
           onClick={(e) => {
