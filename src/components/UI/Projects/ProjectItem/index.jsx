318e0bc8754c7250adb376c7e50d8a22916ecd3f
Bobur_Sunnatov <bsunnatov135@gmail.com>
2023-02-06 10:26:58 +0500

    bug fix

--- src/components/UI/Projects/ProjectItem/index.jsx
+++ src/components/UI/Projects/ProjectItem/index.jsx
@@ -5,7 +5,7 @@ import { DefaultImage } from "/public/icons/icons";
 
 export default function ProjectItem({ item }) {
   const { lang } = useTranslation();
-  console.log(item)
+
   return (
     <div className={styles.item} key={item.id}>
       <Link href={`/project-info/${item.guid}`}>
