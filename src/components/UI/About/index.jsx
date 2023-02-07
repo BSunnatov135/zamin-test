1b4fa2d999fc37ea028e210cbd7ec418521fc18a
muhammadjon_qilichev <muhammadjonqilichev09112002@gmail.com>
2023-02-07 14:50:15 +0500

    styling in about-page

--- src/components/UI/About/index.jsx
+++ src/components/UI/About/index.jsx
@@ -2,20 +2,11 @@ import { Container } from "@mui/material";
 import styles from "./style.module.scss";
 import useTranslation from "next-translate/useTranslation";
 import dynamic from "next/dynamic";
-import useSpheres from "services/spheres";
 
 const Board = dynamic(() => import("./Board"));
 
 export default function About() {
   const { t } = useTranslation("about");
-  const { lang } = useTranslation();
-  const { spheres, sphere } = useSpheres({
-    sphereParams: {
-      offset: 0,
-      limit: 3,
-    },
-  });
-  console.log("sphres", spheres);
   return (
     <>
       <h2 className={styles.title}>{t("about")}</h2>
@@ -24,43 +15,31 @@ export default function About() {
       </div>
       <Container>
         <div className={styles.context}>
-          <p className={styles.aim}>{t("aim")}</p>
-          <p className={styles.isAbout}>{t("focus")}</p>
-          <p className={styles.items}>{t("monitor")}</p>
-          <p className={styles.items}>{t("development")}</p>
-          <p className={styles.items}>{t("condition")}</p>
-          <p className={styles.naming}>{t("name")}</p>
+          <p>{t("aim")}</p>
+          <p>{t("focus")}</p>
+          <p>{t("monitor")}</p>
+          <p>{t("development")}</p>
+          <p>{t("condition")}</p>
+          <p>{t("name")}</p>
         </div>
         <div id="sphere">
           <h2 className={styles.sphereTitle}>{t("spheres")}</h2>
           <div className={styles.spheresWrapper}>
             <div className={styles.sphereItem}>
-              <img src={spheres?.data?.response?.[0].photo}></img>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: spheres?.data?.response?.[0][`${lang}_name`],
-                }}
-              />
+              <img src="/images/sphere1.jpeg"></img>
+              <p>{t("environment")}</p>
             </div>
             <div className={styles.sphereItem}>
-              <img src={spheres?.data?.response?.[1].photo}></img>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: spheres?.data?.response?.[1][`${lang}_name`],
-                }}
-              />
+              <img src="/images/sphere2.jpeg"></img>
+              <p>{t("innovation")} </p>
             </div>
             <div className={styles.sphereItem}>
-              <img src={spheres?.data?.response?.[2].photo}></img>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: spheres?.data?.response?.[2][`${lang}_name`],
-                }}
-              />
+              <img src="/images/sphere3.jpeg"></img>
+              <p>{t("program")} </p>
             </div>
           </div>
         </div>
-        <Board />
+        <Board id="board" />
       </Container>
     </>
   );
