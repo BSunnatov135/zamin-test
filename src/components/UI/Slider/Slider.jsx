30439a0bc19d9bcd27c56dc1c6fdb93f016c137b
Bobur_Sunnatov <bsunnatov135@gmail.com>
2023-02-09 11:53:37 +0500

    slider fixing

--- src/components/UI/Slider/Slider.jsx
+++ src/components/UI/Slider/Slider.jsx
@@ -2,33 +2,41 @@ import React, { useState } from "react";
 import styles from "./style.module.scss";
 import BtnSlider from "./BtnSlider/BtnSlider";
 import { useMemo } from "react";
+import Slider from "react-slick";
 
-export default function Slider({ data, title }) {
-  const [slideIndex, setSlideIndex] = useState(1);
-
-  console.log("slideIndex", slideIndex);
-
-  const nextSlide = () => {
-    if (slideIndex < data.length) {
-      setSlideIndex(slideIndex + 1);
-    } else {
-      setSlideIndex(1);
-    }
+export default function InfoSlider({ data, title }) {
+  const settings = {
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
-  const prevSlide = () => {
-    if (slideIndex !== 1) {
-      setSlideIndex(slideIndex - 1);
-    } else if (slideIndex === 1) {
-      setSlideIndex(data.length);
-    }
-  };
-  const newArr = [];
-  const currentData = useMemo(() => {
-    data.filter((el) => el.file_link && newArr?.push(el));
-    return newArr[slideIndex - 1];
-  }, [slideIndex, newArr, data]);
+  // const [slideIndex, setSlideIndex] = useState(1);
 
-  console.log("currentData", currentData);
+  // console.log("slideIndex", data);
+
+  // const nextSlide = () => {
+  //   if (slideIndex < data.length) {
+  //     setSlideIndex(slideIndex + 1);
+  //   } else {
+  //     setSlideIndex(1);
+  //   }
+  // };
+  // const prevSlide = () => {
+  //   if (slideIndex !== 1) {
+  //     setSlideIndex(slideIndex - 1);
+  //   } else if (slideIndex === 1) {
+  //     setSlideIndex(data.length);
+  //   }
+  // };
+  // const newArr = [];
+  // const currentData = useMemo(() => {
+  //   data.filter((el) => el.file_link && newArr?.push(el));
+  // }, [newArr, data]);
+  // console.log("newArr", newArr);
+  console.log("data", data);
   return (
     <>
       <h2
@@ -37,10 +45,10 @@ export default function Slider({ data, title }) {
           __html: title,
         }}
       />
-      {currentData &&
-        (newArr.length > 1 ? (
+      {data &&
+        (data.length > 1 ? (
           <>
-            <div className={styles.containerSlider}>
+            {/* <div className={styles.containerSlider}>
               {data.map((obj, index) => {
                 return (
                   <>
@@ -86,11 +94,12 @@ export default function Slider({ data, title }) {
                   key={`dots-${index}`}
                 />
               ))}
-            </div>
+            </div> */}
+            <Slider {...settings} className={styles.containerSlider}></Slider>
           </>
-        ) : newArr.length === 1 ? (
+        ) : data.length === 1 ? (
           <div className={styles.singleElement}>
-            {currentData?.type?.toLowerCase() === "mp4" ? (
+            {data?.type?.toLowerCase() === "mp4" ? (
               <video
                 loop
                 playsInline
@@ -100,13 +109,10 @@ export default function Slider({ data, title }) {
                 height={"100%"}
                 className={styles.singleElement}
               >
-                <source src={currentData?.file_link} type="video/mp4" />
+                <source src={data?.file_link} type="video/mp4" />
               </video>
             ) : (
-              <img
-                src={currentData?.file_link}
-                className={styles.singleElement}
-              />
+              <img src={data.file_link} className={styles.singleElement} />
             )}
           </div>
         ) : (
