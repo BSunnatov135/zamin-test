80047e4ba329e12d28e780ef063a441091e134c6
Bobur_Sunnatov <bsunnatov135@gmail.com>
2023-02-15 12:04:14 +0500

    projects bug fix

--- src/components/UI/Projects/ProjectItem/index.jsx
+++ src/components/UI/Projects/ProjectItem/index.jsx
@@ -6,32 +6,57 @@ import { DefaultImage } from "/public/icons/icons";
 export default function ProjectItem({ item }) {
   const { lang } = useTranslation();
   return (
-    <div className={styles.item} key={item.guid}>
-      <Link href={`/project-info/${item.guid}`}>
-        <a className={styles.item__content}>
+    <Link href={`/project-info/${item.guid}?from=events`}>
+      <a>
+        <div className={styles.item} key={item.img}>
           <div className={styles.img}>
-            {item[`${lang}_photo`] ? (
-              <img src={item[`${lang}_photo`]} alt={item[`${lang}_header`]} />
-            ) : (
-              <DefaultImage className={styles.defaultImg} />
-            )}
+            <img src={item[`${lang}_photo`]} alt={item[`${lang}_header`]} />
           </div>
           <div className={styles.body}>
             <p
-              dangerouslySetInnerHTML={{
-                __html: item[`${lang}_name`] ? item[`${lang}_name`] : item[`${lang}_header`],
-              }}
               className={styles.body__title}
+              dangerouslySetInnerHTML={{
+                __html: item[`${lang}_name`]
+                  ? item[`${lang}_name`]
+                  : item[`${lang}_header`],
+              }}
             />
             <p
-              dangerouslySetInnerHTML={{
-                __html: item[`${lang}_description`],
-              }}
               className={styles.body__text}
-            ></p>
+              dangerouslySetInnerHTML={{
+                __html: item?.[`${lang}_description`],
+              }}
+            />
           </div>
-        </a>
-      </Link>
-    </div>
+        </div>
+      </a>
+    </Link>
+    // <div className={styles.item} key={item.guid}>
+    //   <Link href={`/project-info/${item.guid}`}>
+    //     <a className={styles.item__content}>
+    //       <div className={styles.img}>
+    //         {item[`${lang}_photo`] ? (
+    //           <img src={item[`${lang}_photo`]} alt={item[`${lang}_header`]} />
+    //         ) : (
+    //           <DefaultImage className={styles.defaultImg} />
+    //         )}
+    //       </div>
+    //       <div className={styles.body}>
+    //         <p
+    //           dangerouslySetInnerHTML={{
+    //             __html: item[`${lang}_name`] ? item[`${lang}_name`] : item[`${lang}_header`],
+    //           }}
+    //           className={styles.body__title}
+    //         />
+    //         <p
+    //           dangerouslySetInnerHTML={{
+    //             __html: item[`${lang}_description`],
+    //           }}
+    //           className={styles.body__text}
+    //         ></p>
+    //       </div>
+    //     </a>
+    //   </Link>
+    // </div>
   );
 }
