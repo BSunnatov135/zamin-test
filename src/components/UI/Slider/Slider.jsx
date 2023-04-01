11bb343d46d0051f504f9ffffedf54a28a18d8d8
Sunnatov Bobur Baxodir o'g'li <bsunnatov135@gmail.com>
2023-04-01 15:43:13 +0500

    slider dots removed

--- src/components/UI/Slider/Slider.jsx
+++ src/components/UI/Slider/Slider.jsx
@@ -17,8 +17,15 @@ export default function InfoSlider({ data, title, queryFrom }) {
     slidesToScroll: 1,
     nextArrow: <RightArrow />,
     prevArrow: <LeftArrow />,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: false,
+        },
+      },
+    ],
   };
-
   // const [slideIndex, setSlideIndex] = useState(1);
 
   // console.log("slideIndex", data);
