814c3f5678e6c7ae30941d3bd903dce2de1d7f1a
Sunnatov Bobur Baxodir o'g'li <bsunnatov135@gmail.com>
2023-05-31 16:39:21 +0500

    bug fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,7 +24,7 @@ export default function Document() {
           //   __html: `(function(d){var s = d.createElement("script");s.setAttribute("data-account", "BVDvm0tQ0l");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)`,
           // }}
           dangerouslySetInnerHTML={{
-            __html: `(function(d){var s = d.createElement("script");s.setAttribute("data-account", "BVDvm0tQ0l");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)`,
+            __html: `(function(d){var s = d.createElement("script");s.setAttribute("data-account", "CZDyCvecmY");s.setAttribute("src", "https://cdn.userway.org/widget.js");(d.body || d.head).appendChild(s);})(document)`,
           }}
         ></script>
       </body>
